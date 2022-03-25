d0eb5ee411f2374c4a51fe3016025b434d0e7da0
Alain Baxter <alain.baxter@sada.com>
2022-03-25 11:32:22 -0400

    Adding scripts to setup demo environment, adding config sync demo initial structure

---
+++ destroy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+help() {
+  echo "Destroy ACM Demo environment"
+  echo
+  echo "Syntax: destroy.sh {project-id} {zone}"
+  echo "where:"
+  echo "  project-id  is the project where the environment will be deployed"
+  echo "  zone        is zone where the environment will be deployed"
+  echo
+}
+
+# These correspond to the clusters in `config-sync-root/cluster-registry`
+CLUSTERS=("acm-demo-dev" "acm-demo-stage" "acm-demo-prod")
+
+if [ -z "$1" ] | [ -z "$2" ]; then
+  echo "Missing Arguments"
+  echo
+  help
+  exit 1
+fi
+
+DEMO_PROJECT_ID=$1
+DEMO_ZONE=$2
+
+for CLUSTER_NAME in ${CLUSTERS[@]}; do
+  echo "Deleting Cluster: $CLUSTER_NAME"
+  gcloud beta container clusters delete $CLUSTER_NAME --zone $DEMO_ZONE --project $DEMO_PROJECT_ID --quiet
+  
+  echo "Unregistering Cluster: $CLUSTER_NAME"
+  gcloud container hub memberships unregister $CLUSTER_NAME --project $DEMO_PROJECT_ID
+done
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+help() {
+  echo "Setup ACM Demo environment"
+  echo
+  echo "Syntax: setup.sh {project-id} {zone}"
+  echo "where:"
+  echo "  project-id  is the project where the environment will be deployed"
+  echo "  zone        is zone where the environment will be deployed"
+  echo
+}
+
+# These correspond to the clusters in `config-sync-root/cluster-registry`
+CLUSTERS=("acm-demo-dev" "acm-demo-stage" "acm-demo-prod")
+
+if [ -z "$1" ] | [ -z "$2" ]; then
+  echo "Missing Arguments"
+  echo
+  help
+  exit 1
+fi
+
+DEMO_PROJECT_ID=$1
+DEMO_ZONE=$2
+
+for CLUSTER_NAME in ${CLUSTERS[@]}; do
+  echo "Creating Cluster: $CLUSTER_NAME"
+  gcloud beta container clusters create $CLUSTER_NAME --zone $DEMO_ZONE --project $DEMO_PROJECT_ID \
+    --preemptible --num-nodes "2" --machine-type "e2-standard-4" --enable-ip-alias \
+    --workload-pool "${DEMO_PROJECT_ID}.svc.id.goog" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM 
+  
+  echo "Registering Cluster: $CLUSTER_NAME"
+  gcloud container hub memberships register $CLUSTER_NAME --project $DEMO_PROJECT_ID \
+    --gke-cluster $DEMO_ZONE/$CLUSTER_NAME --enable-workload-identity
+  
+  #echo "Enabling ACM: $CLUSTER_NAME"
+  #gcloud beta container hub config-management apply --project $PROJECT_ID \
+  #  --membership $CLUSTER_NAME --config apply-spec.yaml
+done
